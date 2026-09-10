9bfd60874a09db5cb5d49b1e39854e34934b8ec4
Sanguinete1822 <rafasang1822@gmail.com>
2026-09-10 11:07:31 -0300

    ADD oficials

---
+++ vPI1.3.py
@@ -0,0 +1,77 @@
+import random
+import tkinter as tk
+janela = tk.Tk()
+BT = tk.Button
+botoes = []
+plam = cpuam = 0
+plays = 0
+n = 1
+def exit():
+    janela.destroy()
+def jogar(escolha):
+    atualizar2("Escolha:")
+    global plam, cpuam, plays
+    if escolha == 'atirar':
+        if plam > 0 and cpuam > 0:
+            plays = ['defender'] * 14 + ['recarregar'] * 1 + ['atirar'] * 3
+        elif plam > 0 and cpuam == 0:
+            plays = ['defender'] * 14 + ['recarregar'] * 3 + ['atirar'] * 1
+        elif plam == 0 and cpuam > 0:
+            plays = ['defender'] * 1 + ['recarregar'] * 3 + ['atirar'] * 14
+        elif plam == 0 and cpuam == 0:
+            plays = ['defender'] * 3 + ['recarregar'] * 14 + ['atirar'] * 1
+    elif escolha == 'recarregar':
+        if cpuam == 0:
+            plays = ['defender'] * 3 + ['recarregar'] * 14 + ['atirar'] * 1
+        else:
+            plays = ['defender'] * 1 + ['recarregar'] * 3 + ['atirar'] * 14
+    else:
+        plays = ['defender'] * 3 +['recarregar'] * 14 +['atirar'] * 1
+    pc = random.choice(plays)
+    if escolha == 'recarregar': plam += 1
+    if pc == 'recarregar': cpuam += 1
+    if escolha == 'atirar':
+        if plam > 0:
+            plam -= 1
+            if pc != 'defender': return fim("Jogador venceu")
+        elif pc == 'atirar':
+            if cpuam > 0:
+                fim("Os dois perderam")
+        else: atualizar2("Sem Munição!!")
+    if pc == 'atirar':
+        if cpuam > 0:
+            cpuam -= 1
+            if escolha != 'defender': return fim("Computador venceu")
+        elif escolha == 'atirar':
+            if plam > 0:
+                fim("Os dois perderam")
+        else: atualizar2("Computador sem Munição!!")
+    atualizar(f"Você:{escolha} | PC:{pc}   munição {plam}/{cpuam}")
+    global n
+    print("Rodada" , n)
+    n += 1
+    print(f"Você: {escolha} | PC: {pc}")
+    print(f"munição {plam}/{cpuam}")
+    return n
+def atualizar(txt):
+    label.config(text=txt)
+def atualizar2(txt):
+    label2.config(text=txt)
+def fim(txt):
+    label.config(text=txt)
+    for b in botoes:
+        b.config(state="disabled")
+titulo = janela.title("A.R.D.")
+janela.geometry("300x300")
+label = tk.Label(janela, text="Bem Vindo")
+label.pack()
+label2 = tk.Label(janela, text="Escolha:")
+label2.pack()
+jogadas = ['atirar','recarregar','defender' ]
+for t in jogadas:
+    b = BT(janela, text=t, command = lambda x=t: jogar(x))
+    botoes.append(b)
+    b.pack()
+botaosaida = BT(text="Parar tudo", command=exit )
+botaosaida.pack()
+janela.mainloop()
